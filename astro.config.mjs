b7ca70b7e5f1d896ccc48265fe76652e97ec4453
8mitsuboy <hachimitsu8897@icloud.com>
2026-03-27 16:23:31 +0900

    @astrojs/mdxインテグレーションを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig, passthroughImageService } from "astro/config";
 import preact from "@astrojs/preact";
+import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bzine.netlify.app/",
-  integrations: [preact()],
+  integrations: [preact(), mdx()],
   image: {
     service: passthroughImageService(),
   },
